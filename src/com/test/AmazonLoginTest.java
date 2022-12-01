da4142e88b9b916afdc7068da3b9ca193165ccd4
amarpadavath <amarpadavath@gmail.com>
2022-12-01 22:07:59 +0530

    updating with pom.xml

--- src/com/test/AmazonLoginTest.java
+++ src/com/test/AmazonLoginTest.java
@@ -1,5 +1,20 @@
 package com.test;
 
-public class AmazonLoginTest {
+import org.testng.annotations.Test;
 
+public class AmazonLoginTest {
+	
+	@Test
+	
+	public void LoginApplication()
+	
+	{
+		System.out.println("loginTestCase");
+	}
+ @Test
+ 
+ public void AddToCart()
+ {
+	 
+ }
 }
